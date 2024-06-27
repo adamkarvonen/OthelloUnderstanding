70a7b51dafd46578353434d07be25d96a9f9f6c7
canrager <canrager@gmail.com>
2024-06-26 20:37:03 -0400

    added coeff visualization

--- neuron_inputs/neuron_input_dataset.py
+++ neuron_inputs/neuron_input_dataset.py
@@ -11,6 +11,7 @@ from collections import defaultdict
 
 from circuits.utils import othello_hf_dataset_to_generator, get_model, get_aes
 import circuits.othello_utils as othello_utils
+import feature_viz_othello_utils
 
 from circuits.eval_sae_as_classifier import construct_othello_dataset
 
@@ -18,8 +19,9 @@ from circuits.eval_sae_as_classifier import construct_othello_dataset
 device = "cuda:0"
 # device = "cpu"
 repo_dir = "/share/u/can/chess-gpt-circuits"
-repo_dir = "/home/adam/OthelloUnderstanding/"
+# repo_dir = "/home/adam/OthelloUnderstanding/"
 tracer_kwargs = {"scan": False, "validate": False}
+seed = 0
 
 # Import model
 model_name = "Baidicoot/Othello-GPT-Transformer-Lens"
@@ -65,7 +67,7 @@ for batch_idx in range(n_batches):
                 model.blocks[layer].mlp.hook_post.output.save()
             )  # could reduce to a smaller datatype eg b float16
 
-# %%'
+# %%
 
 for layer in neuron_acts:
     neuron_acts[layer] = t.stack(neuron_acts[layer])
@@ -89,12 +91,13 @@ print(X.shape, y.shape)
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression, Lasso
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
+#%%
 
 # Function to train and evaluate a model
 def train_and_evaluate(model, X_train, X_test, y_train, y_test):
@@ -107,45 +110,26 @@ def train_and_evaluate(model, X_train, X_test, y_train, y_test):
 
 # Linear Regression
 linear_reg = LinearRegression()
+linear_reg = Lasso(alpha=0.01, random_state=seed)
 linear_mse, linear_r2 = train_and_evaluate(linear_reg, X_train, X_test, y_train, y_test)
 print(f"Linear Regression - MSE: {linear_mse}, R2: {linear_r2}")
 
-# # Decision Tree
-# dt_reg = MultiOutputRegressor(DecisionTreeRegressor(random_state=42))
-# dt_mse, dt_r2 = train_and_evaluate(dt_reg, X_train, X_test, y_train, y_test)
-# print(f"Decision Tree - MSE: {dt_mse}, R2: {dt_r2}")
+# %%
+# Get the coefficients for neuron 421
+neuron_421_coeffs = linear_reg.coef_[421]
+neuron_421_coeffs = t.tensor(neuron_421_coeffs, device=device, dtype=t.float32)
+# Get the intercept for neuron 421
+neuron_421_intercept = linear_reg.intercept_[421]
+print(f"Coefficients for neuron 421: {neuron_421_coeffs}")
+print(f"Intercept for neuron 421: {neuron_421_intercept}")
 
-# # Random Forest
-# rf_reg = MultiOutputRegressor(RandomForestRegressor(n_estimators=100, random_state=42))
-# rf_mse, rf_r2 = train_and_evaluate(rf_reg, X_train, X_test, y_train, y_test)
-# print(f"Random Forest - MSE: {rf_mse}, R2: {rf_r2}")
+# %%
+fig, ax = plt.subplots()
+feature_viz_othello_utils.visualize_vocab(ax, neuron_421_coeffs[:60], device)
+ax.set_title("Neuron 421 Coefficients most recent")
 
-# Regression tensor of shape
-"""
-b: batch_size, number of games
-L: sequence length, number of moves per game
-C: cell/square the most recent move was played on (J for just played) ++ all cells/squares that are occupied, all moves in the token string prior the most recent move
-"""
-
-# from sklearn.linear_model import LinearRegression
-# from sklearn.metrics import mean_squared_error, r2_score
-
-# # Model training
-# model = LinearRegression()
-# model.fit(X_train, y_train)
-
-# # Predictions
-# y_pred = model.predict(X_test)
-
-# # Evaluation
-# mse = mean_squared_error(y_test, y_pred)
-# r2 = r2_score(y_test, y_pred)
-
-
-"""
-othello utils
-get data batch
-collect activations batch
-
-"""
+# %%
+fig, ax = plt.subplots()
+feature_viz_othello_utils.visualize_vocab(ax, neuron_421_coeffs[60:120], device)
+ax.set_title("Neuron 421 Coefficients any past moves except the most recent")
 # %%
